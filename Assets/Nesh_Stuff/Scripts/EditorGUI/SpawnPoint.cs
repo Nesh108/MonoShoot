31408ae8aaf38c4dacecd94d2814b7146db950c6
unknown <alberto.vaccari108@gmail.com>
2015-11-24 13:14:26 +0200

    added spawnpoint gizmos for visibility

---
+++ Assets/Nesh_Stuff/Scripts/EditorGUI/SpawnPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour
+{
+	public Mesh spawnMesh;
+	public Color color = Color.red;
+
+	public void OnDrawGizmos ()
+	{
+		Gizmos.color = color;
+		Gizmos.DrawMesh(spawnMesh, transform.position);
+	}
+
+}
