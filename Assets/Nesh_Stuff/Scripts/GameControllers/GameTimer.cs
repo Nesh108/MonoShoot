cc6c8bced351c6c8f6a6c657e324ed3ff5c0713f
unknown <alberto.vaccari108@gmail.com>
2015-11-25 21:25:25 +0200

    added day and night cycle with sunrise and sunset

--- Assets/Nesh_Stuff/Scripts/GameControllers/GameTimer.cs
+++ Assets/Nesh_Stuff/Scripts/GameControllers/GameTimer.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 public class GameTimer : MonoBehaviour
 {
+	public enum TimeOfDay
+	{
+		Idle,
+		SunRise,
+		SunSet
+	}
+
 	public Transform[] suns;
-	private Sun[] _sunScript;
 	public float dayCycleInMinutes = 1f;
+	public float sunRise;					// Time of day for sunrise
+	public float sunSet;					// Time of day for sunset
+	public float skyboxBlendModifier;		// The speed at which the textures in the skyboxes blend
+
+	private Sun[] _sunScript;
 	private const float SECOND = 1f;
 	private const float MINUTE = 60 * SECOND;
 	private const float HOUR = 60 * MINUTE;
@@ -13,10 +24,20 @@ public class GameTimer : MonoBehaviour
 	private const float DEGREES_PER_SECOND = 360 / DAY;
 	private float _degreeRotation;
 	private float _timeOfDay;
+	private float _dayCycleInSeconds;
+	private int _daysPassed = 0;
+	private TimeOfDay _tod;
 
 	// Use this for initialization
 	void Start ()
 	{
+
+		_tod = TimeOfDay.Idle;
+
+		_dayCycleInSeconds = dayCycleInMinutes * MINUTE;
+
+		RenderSettings.skybox.SetFloat ("_Blend", 0);
+
 		_sunScript = new Sun[suns.Length];
 
 		for (int i = 0; i < suns.Length; i++) {
@@ -32,7 +53,11 @@ public class GameTimer : MonoBehaviour
 			_sunScript [i] = tempSunScript;
 		}
 		_timeOfDay = 0;
-		_degreeRotation = DEGREES_PER_SECOND * DAY / (dayCycleInMinutes * MINUTE);
+		_degreeRotation = DEGREES_PER_SECOND * DAY / _dayCycleInSeconds;
+
+		// Convert values to seconds
+		sunRise *= _dayCycleInSeconds; 
+		sunSet *= _dayCycleInSeconds; 
 	}
 	
 	// Update is called once per frame
@@ -42,6 +67,43 @@ public class GameTimer : MonoBehaviour
 			suns [i].Rotate (new Vector3 (_degreeRotation, 0, 0) * Time.deltaTime);
 
 		_timeOfDay += Time.deltaTime;
-		Debug.Log (_timeOfDay);
+
+		// Reset clock and add a day
+		if (_timeOfDay > _dayCycleInSeconds) {
+			_timeOfDay -= _dayCycleInSeconds;
+			_daysPassed++;
+		}
+
+		if (_timeOfDay > sunRise && _timeOfDay < sunSet && RenderSettings.skybox.GetFloat ("_Blend") < 1) {
+			_tod = TimeOfDay.SunRise;
+			BlendSkybox ();
+		} else if (_timeOfDay > sunSet && RenderSettings.skybox.GetFloat ("_Blend") > 0) {
+			_tod = TimeOfDay.SunSet;
+			BlendSkybox ();
+		} else {
+			_tod = TimeOfDay.Idle;
+		}
+
 	}
+
+	private void BlendSkybox ()
+	{
+		float time = 0;
+
+		switch (_tod) {
+		case TimeOfDay.SunRise: 
+			time = (_timeOfDay - sunRise) / _dayCycleInSeconds * skyboxBlendModifier;
+			break;
+		case TimeOfDay.SunSet:
+			time = (_timeOfDay - sunSet) / _dayCycleInSeconds * skyboxBlendModifier;
+			time = 1 - time;
+			
+			break;
+		}
+
+		RenderSettings.skybox.SetFloat ("_Blend", time);
+
+		Debug.Log (time);
+	} 
+
 }
